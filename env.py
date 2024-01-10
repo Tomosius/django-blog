d86db931f638643627f386963f165888c53f08c2
Tomas Pecukevicius <pecukevicius@gmail.com>
2024-01-10 22:40:51 +0000

    deployment settings

---
+++ env.py
@@ -0,0 +1,4 @@
+import os
+
+os.environ.setdefault("DATABASE_URL", "postgres://ogmsulil:2FhDjw-rBDu-qcQ1s7jwk0ClANWRBKOO@heffalump.db.elephantsql"
+                                      ".com/ogmsulil")
