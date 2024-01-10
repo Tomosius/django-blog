8238666a4553fd3e32c626f7f9630debf3eabc4a
Tomas Pecukevicius <pecukevicius@gmail.com>
2024-01-10 23:42:14 +0000

    added secret key

--- codestar/settings.py
+++ codestar/settings.py
@@ -26,12 +26,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-)vvd&!n_e5e8^12cvq^&g!l5^&*1n*-g^w67p()*ri=sj%_qg8'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['.herokuapp.com']
+ALLOWED_HOSTS = ['127.0.0.1','.herokuapp.com']
 
 
 # Application definition
--- env.py
+++ env.py
@@ -1,4 +1,5 @@
 import os
 
-os.environ.setdefault("DATABASE_URL", "postgres://ogmsulil:2FhDjw-rBDu-qcQ1s7jwk0ClANWRBKOO@heffalump.db.elephantsql"
-                                      ".com/ogmsulil")
+os.environ.setdefault("DATABASE_URL", "postgres://ogmsulil:2FhDjw-rBDu-qcQ1s7jwk0ClANWRBKOO@heffalump.db.elephantsql.com/ogmsulil")
+
+os.environ.setdefault("SECRET_KEY", "9-))_vw0q6xj#zw27_0r+_n$+6(!#nr&zgu#0x06rtl$m)nu@8")
