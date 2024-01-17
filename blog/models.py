8c5e8b54ce613a91d20519c931e970084f12ce53
Tomas Pecukevicius <pecukevicius@gmail.com>
2024-01-17 20:36:42 +0000

    generic post view

--- blog/models.py
+++ blog/models.py
@@ -2,8 +2,11 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
-STATUS = ((0,"Draft"),(1,"Published"))
+STATUS = ((0, "Draft"), (1, "Published"))
+
 # Create your models here.
+
+
 class Post(models.Model):
     title = models.CharField(max_length=200, unique=True)
     slug = models.SlugField(max_length=200, unique=True)
@@ -15,3 +18,22 @@ class Post(models.Model):
     excerpt = models.TextField(blank=True)
     updated_on = models.DateTimeField(auto_now=True)
 
+    class Meta:
+        ordering = ['-created_on']
+
+    def __str__(self):
+        return f" {self.title} | writen by {self.author}"
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='commenter')
+    body = models.TextField()
+    approved = models.BooleanField(default=False)
+    created_on = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-created_on']
+
+    def __str__(self):
+        return f"Comment {self.body} by {self.author}"
